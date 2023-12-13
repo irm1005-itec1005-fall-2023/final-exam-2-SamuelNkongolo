81f1153c6616849de312d768cb6b592914eaae77
Samuel Nkongolo <samuelnkongolo@cmail.carleton.ca>
2023-12-13 17:45:17 -0500

    Done

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ let time = document.getElementById("current-time");
         let d = new Date();
         time.innerHTML = d.toLocaleTimeString();
     },1000)
-  
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // Function to shuffle the boxes
+    function shuffleBoxes() {
+      var boxesContainer = document.querySelector('.boxes');
+      for (var i = boxesContainer.children.length; i >= 0; i--) {
+        boxesContainer.appendChild(boxesContainer.children[Math.random() * i | 0]);
+      }
+    }
+
+    // Add click event listener to the shuffle button
+    var shuffleButton = document.getElementById('shuffle-button');
+    if (shuffleButton) {
+      shuffleButton.addEventListener('click', shuffleBoxes);
+    }
+  });
